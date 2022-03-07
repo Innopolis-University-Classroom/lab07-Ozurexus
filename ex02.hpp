f7786c754af01a94fc9c9b9818902cfc76e92d74
MaximPinyaguin <88331457+Ozurexus@users.noreply.github.com>
2022-03-07 13:36:47 +0300

    Update ex02.hpp

--- ex02.hpp
+++ ex02.hpp
@@ -2,23 +2,18 @@
 // the function below now only works with integers, change it to work with any type
 // also fill out the body
 
-public class Calculator {
-
+template<typename T>
+ public class Calculator {
     private:
-        int num1, num2;  
+        int num1, num2;
     public:
-        template <typename T>
         Calculator(T n1, T n2) {
             num1 = n1;
             num2 = n2;
         }
-        template <typename T>
         int add(T num1,T num2) { return num1+num2;}
-        template <typename T>
         int subtract(T num1,T num2) {return num1-num2;}
-        template <typename T>
         int multiply(T num1,T num2) { return num1*num2;}
-        template <typename T>
         int divide(T num1,T num2) { return num1/num2;}
     };
 
