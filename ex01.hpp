bc52209135f46c9588e09f4902f9c43be5851a78
MaximPinyaguin <88331457+Ozurexus@users.noreply.github.com>
2022-03-07 13:46:04 +0300

    Update ex01.hpp

--- ex01.hpp
+++ ex01.hpp
@@ -3,7 +3,7 @@
 // the function below now only works with integers, change it to work with any type
 // also fill out the body
 template <typename WhatKind>
-void multiples (WhatKind sum, WhatKind x, int n)
+void multiples (WhatKind& sum, WhatKind x, int n)
 {
     sum++;
     for(int i=0;i<n+1;i++)
